8dbdd0b35fc734cf35a5ee69377649385e594467
Alper Tolga Kocataş <alperkocatas@gmail.com>
2022-10-13 10:46:41 +0300

    added missing alf directory

---
+++ AlfRefImp-1.1.0k/dist/alf/alf.bat
@@ -0,0 +1,15 @@
+@rem ***************************************************************************
+@rem Copyright 2011-2015 Data Access Technologies, Inc. (Model Driven Solutions)
+@rem
+@rem All rights reserved worldwide. This program and the accompanying materials
+@rem are made available for under the terms of the GNU General Public License 
+@rem (GPL) version 3 that accompanies this distribution and is available at 
+@rem http://www.gnu.org/licenses/gpl-3.0.html.
+@rem ***************************************************************************
+@echo off
+
+set lib=
+if not "%ALF_LIB%" == "" set lib=-l %ALF_LIB%
+if "%*" == "-v" set lib=
+
+java -jar dist/alf.jar %lib% %*
